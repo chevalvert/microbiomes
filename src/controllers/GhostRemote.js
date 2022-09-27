38288fc61fd709f0310db62b8417513c861fd069
Arnaud Juracek <arnaud.juracek@gmail.com>
2022-09-27 16:51:10 +0200

    Reset ghost remote timer on remote events

--- src/controllers/GhostRemote.js
+++ src/controllers/GhostRemote.js
@@ -1,14 +1,18 @@
 import Population from 'controllers/Population'
 import { randomInt } from 'missing-math'
+import WebSocketServer from 'controllers/WebSocketServer'
 
 let timer
 let isRunning = false
 
+WebSocketServer.content.subscribe(reset)
+
 function tick () {
+  const next = randomInt(window.ENV.ghostRemote[0], window.ENV.ghostRemote[1])
   timer = window.setTimeout(() => {
     Population.add(Population.createRandomCreature())
     tick()
-  }, randomInt(window.ENV.ghostRemote[0], window.ENV.ghostRemote[1]))
+  }, next)
 }
 
 export function start () {
@@ -17,9 +21,15 @@ export function start () {
   tick()
 }
 
+export function reset () {
+  if (!isRunning) return
+  stop()
+  start()
+}
+
 export function stop () {
   isRunning = false
   window.clearTimeout(timer)
 }
 
-export default { start, stop }
+export default { start, reset, stop }
